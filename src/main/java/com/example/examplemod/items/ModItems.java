b879f75bb93dda42126e79ddf4c78ac47c00fa5b
Aurora <139906198+aurora-1-2-3@users.noreply.github.com>
2025-07-14 19:49:51 +0500

    ff

---
+++ src/main/java/com/example/examplemod/items/ModItems.java
@@ -0,0 +1,23 @@
+package com.example.examplemod.items;
+
+import com.example.examplemod.FirstMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+      public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FirstMod.MODID);
+
+
+      public static final DeferredItem<Item> GALLIUM = ITEMS.register("gallium", () -> new Item(new Item.Properties()));
+
+
+
+      public static void register(IEventBus eventBus){
+          ITEMS.register(eventBus);
+
+      }
+
+
+}
