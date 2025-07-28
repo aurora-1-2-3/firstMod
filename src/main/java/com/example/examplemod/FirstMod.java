2d96b318d26cf593a266a3f2b013e599f1227453
Aurora <139906198+aurora-1-2-3@users.noreply.github.com>
2025-07-28 17:33:24 +0500

    fuck me, this is hard

--- src/main/java/com/example/examplemod/FirstMod.java
+++ src/main/java/com/example/examplemod/FirstMod.java
@@ -29,13 +29,8 @@ public class FirstMod {
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public FirstMod(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
         ModCreativeModeTab.register(modEventBus);
 
