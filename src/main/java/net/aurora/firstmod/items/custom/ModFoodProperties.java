5e3e5896d3e85a8fe04cf904ee22122a3d38aff0
Aurora <139906198+aurora-1-2-3@users.noreply.github.com>
2025-10-20 07:54:07 +0500

    idfk

--- src/main/java/net/aurora/firstmod/items/custom/ModFoodProperties.java
+++ src/main/java/net/aurora/firstmod/items/custom/ModFoodProperties.java
@@ -10,7 +10,7 @@ public class ModFoodProperties {
 
     public static final FoodProperties COOKED_AXOLOTL_EGG = new FoodProperties.Builder().nutrition(4).saturationModifier(0.5f)
             .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 4000, 4), 0.5f).build();
-
+//
     public static final FoodProperties NEON_POTATO = new FoodProperties.Builder().nutrition(4).saturationModifier(0.25f)
             .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 1500, 1), 1F).build();
 }
